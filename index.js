51ba6ce478ecc5f0fb49d7105198b4b120235018
Rohan Palankar <rohanpalankar8@gmail.com>
2023-02-04 19:40:24 +0530

    modify file

--- index.js
+++ index.js
@@ -1103,3 +1103,15 @@
 // one of them is removed. This results in {1, 2, 3, 4}.
 
 
+
+
+// 57. What is the output?
+
+// counter.js
+// let counter = 10;
+// export default counter;
+
+// Explaination: 
+// An imported module is read-only: you cannot modify the imported module. Only the module that exports them can change its value.
+
+// When we try to increment the value of myCounter, it throws an error: myCounter is read-only and cannot be modified.
