2b0b3607f79e96c415695a9bb2456d9a84a85d14
Rohan Palankar <rohanpalankar8@gmail.com>
2023-02-11 17:22:35 +0530

    modify file

--- index.js
+++ index.js
@@ -1370,3 +1370,29 @@
 
 
 
+// 69. What is the output?
+
+// const name = "Lydia Hallie"
+// console.log(name.padStart(13))
+// console.log(name.padStart(2))
+
+// Explaination:
+// With the padStart method, we can add padding to the beginning of a string. The value passed to this method is the 
+// total length of the string together with the padding. The string "Lydia Hallie" has a length of 12. name.padStart(13) 
+// inserts 1 space at the start of the string, because 12 + 1 is 13.
+
+// If the argument passed to the padStart method is smaller than the length of the array, no padding will be added.
+
+
+
+
+// 70. What is the output?
+
+// console.log("🥑" + "💻");
+
+// Explaination:
+// With the + operator, you can concatenate strings. In this case, we are concatenating the string "🥑" with the string "💻", resulting in "🥑💻".
+
+
+
+
