84fddc2e4c1c364919b6e16c72e42677f5201e47
Rohan Palankar <rohanpalankar8@gmail.com>
2023-02-12 22:05:26 +0530

    modify file

--- index.js
+++ index.js
@@ -1396,3 +1396,61 @@
 
 
 
+// 71. How can we log the values that are commented out after the console.log statement?
+
+// function* startGame() {
+//   const answer = yield "Do you love JavaScript?";
+//   if (answer !== "Yes") {
+//     return "Oh wow... Guess we're gone here";
+//   }
+//   return "JavaScript loves you back ❤️";
+// }
+
+// const game = startGame();
+// console.log(/* 1 */); // Do you love JavaScript?
+// console.log(/* 2 */); // JavaScript loves you back ❤️
+
+// Explaination:
+// A generator function "pauses" its execution when it sees the yield keyword. First, we have to let the function yield 
+// the string "Do you love JavaScript?", which can be done by calling game.next().value.
+
+// Every line is executed, until it finds the first yield keyword. There is a yield keyword on the first line within the 
+// function: the execution stops with the first yield! This means that the variable answer is not defined yet!
+
+// When we call game.next("Yes").value, the previous yield is replaced with the value of the parameters passed to the next() function, 
+// "Yes" in this case. The value of the variable answer is now equal to "Yes". The condition of the if-statement returns false, 
+// and JavaScript loves you back ❤️ gets logged.
+
+
+
+
+// 72. What is the output?
+
+// console.log(String.raw`Hello\nworld`);
+
+// a. Hello world!
+// b. Hello
+//     world
+// c. Hello\nworld
+// d. Hello\n
+//      world
+
+// Explaination:
+// String.raw returns a string where the escapes (\n, \v, \t etc.) are ignored! Backslashes can be an issue since you could end up with 
+// something like:
+
+// const path = `C:\Documents\Projects\table.html`
+
+// Which would result in:
+
+// "C:DocumentsProjects able.html"
+
+// With String.raw, it would simply ignore the escape and print:
+
+// C:\Documents\Projects\table.html
+
+// In this case, the string is Hello\nworld, which gets logged.
+
+
+
+
