63739d48f58fa96ed9b78eca345e0832c1305459
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-06 10:55:46 +0530

    modify file

--- index.js
+++ index.js
@@ -1980,3 +1980,21 @@
 
 // 96. What is the output?
 
+// class Person {
+//   constructor() {
+//     this.name = "Lydia"
+//   }
+// }
+
+// Person = class AnotherPerson {
+//   constructor() {
+//     this.name = "Sarah"
+//   }
+// }
+
+// const member = new Person()
+// console.log(member.name)
+
+// Explaination:
+// We can set classes equal to other classes/function constructors. In this case, we set Person equal to AnotherPerson. 
+// The name on this constructor is Sarah, so the name property on the new Person instance member is "Sarah".
