51528e774d0b37989eb4fe45f32e9c3d36cab4d9
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-06 11:13:13 +0530

    modify file

--- index.js
+++ index.js
@@ -2020,3 +2020,37 @@
 // accidental name collision on objects, for example when working with 2 libraries that want to add properties 
 // to the same object), you can also "hide" properties on objects this way (although not entirely. You can still 
 // access symbols using the Object.getOwnPropertySymbols() method).
+
+
+
+
+// 98. What is the output?
+
+// const getList = ([x, ...y]) => [x, y]
+// const getUser = user => ({ name: user.name, age: user.age })
+
+// const list = [1, 2, 3, 4]
+// const user = { name: "Lydia", age: 21 }
+
+// console.log(getList(list))
+// console.log(getUser(user))
+
+// Explaination:
+// The getList function receives an array as its argument. Between the parentheses of the getList function, we destructure this array right away. 
+// You could see this as:
+
+// [x, ...y] = [1, 2, 3, 4]
+
+// With the rest parameter ...y, we put all "remaining" arguments in an array. The remaining arguments are 2, 3 and 4 
+// in this case. The value of y is an array, containing all the rest parameters. The value of x is equal to 1 in this case, 
+// so when we log [x, y], [1, [2, 3, 4]] gets logged.
+
+// The getUser function receives an object. With arrow functions, we don't have to write curly brackets if we just return one value. 
+// However, if you want to return an object from an arrow function, you have to write it between parentheses, otherwise no value gets returned! 
+// The following function would have returned an object:
+
+// const getUser = user => ({ name: user.name, age: user.age })
+
+// Since no value gets returned in this case, the function returns undefined.
+
+
