564e2b94a9e341f5686eadf377a34cc427506bc8
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-08 09:51:32 +0530

    modify file

--- index.js
+++ index.js
@@ -2182,3 +2182,29 @@
 // it'll be a resolved promise with the resolved value of that passed promise.
 
 // In this case, we just passed the numerical value 5. It returns a resolved promise with the value 5.
+
+
+
+
+// 105. What is its value?
+
+// function compareMembers(person1, person2 = person) {
+//   if (person1 !== person2) {
+//     console.log("Not the same!")
+//   } else {
+//     console.log("They are the same!")
+//   }
+// }
+
+// const person = { name: "Lydia" }
+
+// compareMembers(person)
+
+// Explaination:
+// Objects are passed by reference. When we check objects for strict equality (===), we're comparing their references.
+
+// We set the default value for person2 equal to the person object, and passed the person object as the value for person1.
+
+// This means that both values have a reference to the same spot in memory, thus they are equal.
+
+// The code block in the else statement gets run, and They are the same! gets logged.
