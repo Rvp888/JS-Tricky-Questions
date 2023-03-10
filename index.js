de3a37f785616c7b0d80bcf60e638d55b67f05ab
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-10 09:59:50 +0530

    modify file

--- index.js
+++ index.js
@@ -2315,3 +2315,37 @@
 // // Stringifying an object  into valid JSON, then parsing the JSON string to a JavaScript value:
 // const jsonArray = JSON.stringify({ name: "Lydia" }) // '{"name":"Lydia"}'
 // JSON.parse(jsonArray) // { name: 'Lydia' }
+
+
+
+
+// 111. What is the output ?
+
+// let name = 'Lydia'
+
+// function getName() {
+//   console.log(name)
+//   let name = 'Sarah'
+// }
+
+// getName()
+
+// Explaination:
+// Each function has its own execution context (or scope). The getName function first looks within its own context (scope) to see if 
+// it contains the variable name we're trying to access. In this case, the getName function contains its own name variable: we declare 
+// the variable name with the let keyword, and with the value of 'Sarah'.
+
+// Variables with the let keyword (and const) are hoisted, but unlike var, don't get initialized. They are not accessible before the line 
+// we declare (initialize) them. This is called the "temporal dead zone". When we try to access the variables before they are declared, 
+// JavaScript throws a ReferenceError.
+
+// If we wouldn't have declared the name variable within the getName function, the javascript engine would've looked down the scope chain. 
+// The outer scope has a variable called name with the value of Lydia. In that case, it would've logged Lydia.
+
+// let name = 'Lydia'
+
+// function getName() {
+//   console.log(name)
+// }
+
+// getName() // Lydia
