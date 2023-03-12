511209c4e4f07ad2425dc4be0a802345e70633bd
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-12 09:33:38 +0530

    modify file

--- index.js
+++ index.js
@@ -2419,3 +2419,30 @@
 // However, since the callback function within setInterval is an arrow function (thus bound to the config object), the callback function 
 // still holds a reference to the config object. As long as there is a reference, the object won't get garbage collected. Since it's 
 // not garbage collected, the setInterval callback function will still get invoked every 1000ms (1s).
+
+
+
+
+// 115. Which method(s) will return the value 'Hello world!' ?
+
+// const myMap = new Map()
+// const myFunc = () => 'greeting'
+
+// myMap.set(myFunc, 'Hello world!')
+
+// //1
+// myMap.get('greeting')
+// //2
+// myMap.get(myFunc)
+// //3
+// myMap.get(() => 'greeting')
+
+// Explaination:
+
+// When adding a key/value pair using the set method, the key will be the value of the first argument passed to the set function, 
+// and the value will be the second argument passed to the set function. The key is the function () => 'greeting' in this case, 
+// and the value 'Hello world'. myMap is now { () => 'greeting' => 'Hello world!' }.
+
+// 1 is wrong, since the key is not 'greeting' but () => 'greeting'. 3 is wrong, since we're creating a new function by passing it 
+// as a parameter to the get method. Object interact by reference. Functions are objects, which is why two functions are never strictly equal, 
+// even if they are identical: they have a reference to a different spot in memory.
